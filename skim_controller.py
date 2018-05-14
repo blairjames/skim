3194d3d310ad3d7013a3d1fe06a5712852ece636
root <blair@blairjames.com>
2018-05-14 17:34:10 +1000

    slowed down for overnight testing

--- skim_controller.py
+++ skim_controller.py
@@ -12,9 +12,9 @@ class SkimController:
         Controller Constructor - Set instance attributes here.
         '''
         try:
-            self.processes: int = 384
+            self.processes: int = 128
             self.http_timeout: int = 60
-            self.staggering: int = 25
+            self.staggering: int = 60
             self.basepath:str = "/root/scripts/skim/output/"
             self.path_to_urls: str = "/root/scripts/skim/master_list_external_domains.txt"
             self.num_domains: int = skim_utils.SkimUitls().how_many_domains_in_list(self.path_to_urls)
@@ -58,7 +58,7 @@ class SkimController:
         '''
         try:
             import multiprocessing
-            with multiprocessing.Pool(int(self.processes), maxtasksperchild=48) as pool:
+            with multiprocessing.Pool(int(self.processes), maxtasksperchild=32) as pool:
                 pool.map(self.director, clean_master_list)
             return True
         except AssertionError as i:
