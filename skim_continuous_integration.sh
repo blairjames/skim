94cb5ff9e485f53ad134ab398e20d5ee29a5dfa9
root <root@localhost.localdomain>
2018-05-15 13:20:39 +1000

    Add shell script for CI

---
+++ skim_continuous_integration.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+echo "stopping systemd_monitor.service"
+systemctl stop systemd_monitor.service
+echo "stopping skim.service"
+systemctl stop skim
+echo "Running git pull"
+cd /root/scripts/skim/
+/usr/bin/git pull
+/usr/bin/git reset --hard master
+/usr/bin/git pull
+echo "starting systemd_monitor.service"
+systemctl start systemd_monitor.service
